187c75c35016b7a86cf5842dd29fc7c886fb41d3
soukaina-ui <zakani.soukaina@gmail.com>
2023-04-25 12:17:54 -0700

    1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_alphabet - Prints the alphabet in lowercase
+  *
+  * Return: Always 0
+  */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
