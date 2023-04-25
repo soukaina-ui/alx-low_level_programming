56b441f51552c8a85c98cbbd6e0019e259c2d84c
soukaina-ui <zakani.soukaina@gmail.com>
2023-04-25 13:40:01 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first int to be added
+ * @b: second int to be added
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
